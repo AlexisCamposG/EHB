e05243bd29d6e515b1c1829958e0e62f6ff15e30
Alexis Campos G <alexisalan.camposgaytan@gmail.com>
2024-02-18 14:54:42 -0600

    vlntie

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,57 @@ var f = ['🔥___________________________________🚒', '🔥___________________
     }
 
     loop();
+
+
+
+
+
+const imagen = document.getElementById("imagen");
+const contador = document.getElementById("contador");
+var basew = imagen.clientWidth;
+var baseh = imagen.clientHeight;
+let clicks = 0;
+
+imagen.addEventListener("click", () => {
+    clicks++;
+    imagen.style.width = `${(imagen.clientWidth)+clicks * 10}px`;
+    imagen.style.height = `${(imagen.clientHeight)+clicks * 10}px`;
+    if (clicks === 10) {
+    
+         clicks = 0;
+        imagen.style.width = `${basew}px`;
+        imagen.style.height = `${baseh}px`;
+        // Crear modal con SweetAlert2
+        Swal.fire({
+            title: '💖💖Happy Valentine💖💖',
+            html: `<video  width="400" video src="/assets/img/106751.mp4" autoplay></video>`,
+            width: 'auto',
+            color: "#716add",
+            background: `
+            url("/assets/img/a12.jpg")
+            left top
+          `,
+            heightAuto : true,
+            loaderHtml: `<div class="spinner-border text-dark"></div>`,
+  customClass: {
+    loader: 'custom-loader'
+  },
+            backdrop: true,
+            confirmButtonText: "xd",
+            allowOutsideClick: false,
+            timer: 37000,
+            timerProgressBar: true,
+            didOpen: () => {
+              Swal.showLoading();
+              const timer = Swal.getPopup().querySelector("b");
+            
+            },
+            willClose: () => {
+              
+            }
+
+
+        });
+    }
+});
+
